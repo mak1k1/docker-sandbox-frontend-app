d7ddef5bf4f341915d029343d5ba1912d5a1465b
Martin Cehelský <martin.cehelsky.ja@gmail.com>
2023-04-08 13:54:06 +0200

    added simple test config

---
+++ src/App.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+
+import App from './App';
+import { describe, it } from 'vitest';
+
+describe('App', () => {
+  it('renders headline', () => {
+    render(<App />);
+
+    screen.debug();
+
+    // check if App components renders headline
+  });
+});
